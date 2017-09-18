facbdaa88fa76af97caba8a578d4c92e30c31138
gitknob <teterski@ualberta.ca>
2017-09-17 22:15:37 -0700

    Began work on file reading

--- assignment1.c
+++ assignment1.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define PROGNAME ("assignment1")
 #define MIN_X 1
@@ -12,31 +13,37 @@ int checkCorrectness(int inst_x, int inst_y);
 int inRange(int val, int min, int max);
 int contains(int array[][2], int x, int y);
 void printValidOptions();
-void saveFile(int array[][2]);
+void openFile(char fileName[]);
+void saveFile(char fileName[]);
 void generateInstances();
 
-int main(){
-	
-	FILE *file = fopen("instance10_001.txt", "r");
+int main(int argc, char *argv[]){
 
-	int inst_x, inst_y;
-	
+	if(argc==3 || argc==5){
 
-	if(file == NULL){
-		generateInstances();
-	} else {
-		if( checkCorrectness(0,0) ){};
-	}
+		if(strcmp(argv[1],"-i") == 0){
+			openFile(argv[2]);
+			if(argc==5 && strcmp(argv[3],"-o")==0){
+				saveFile(argv[4]);
+			}
+
+		}else if(strcmp(argv[1],"-o") == 0){
+			if(argc==3)
+				generateInstances();
+			else if(strcmp(argv[3],"-i") == 0){
+				openFile(argv[2]);
+			}
+			saveFile(argv[4]);
+
+		}else printValidOptions();
+
+	}else printValidOptions();
 
 	return 0;
 }
 
 int checkCorrectness(int inst_x, int inst_y){
-		
-	//if(! inRange() || inRange()
-	//	return false;
-	
-	//continue from here
+
 	return true;
 
 }
@@ -59,7 +66,41 @@ void printValidOptions(){
 	printf("%s [-i inputfile [-o outputfile]]\n",PROGNAME);
 }
 
-void saveFile(int array[][2]){
+void openFile(char fileName[]){
+	
+	FILE *file = fopen(fileName,"r");
+
+	if(file){
+
+		char *token;
+		char str[128], delim=" ";
+
+		//int points[][2];
+		int num_pt, max_x, max_y, mode=0;
+
+		while(fgets(str, sizeof(str), file) != NULL){
+			if(strncmp(str,"#",1) == 0)
+				 continue;
+
+			printf("%s", str);
+
+			if(mode==0){
+				printf("test2");
+				token = strtok(str, delim);
+				printf("test3");
+				printf("x: %s  ", token);
+				token = strtok(NULL, delim);
+				printf("y: %s\n", token);
+				mode++;
+			}else if(mode == 1){
+				mode++;
+			}else{}
+		}
+	} else printf("Error in reading the instance file!\n");
+
+}
+
+void saveFile(char fileName[]){
 	
 }
 
