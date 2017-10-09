c63f435242a4d063fc635a4282bf2b01d5d9a96d
gitknob <teterski@ualberta.ca>
2017-10-08 22:27:18 -0600

    syncing

--- assignment1.c
+++ assignment1.c
@@ -9,34 +9,36 @@
 #define MAX_X 1000
 #define MAX_Y 1000
 
+struct Instance{
+	int x, y, num_pt, *points;
+}instance;
+
+
 int checkCorrectness(int inst_x, int inst_y);
 int inRange(int val, int min, int max);
-int contains(int array[][2], int x, int y);
+int contains(int array[], int x, int y);
 void printValidOptions();
 void openFile(char fileName[], bool print);
 void saveFile(char fileName[]);
 void generateInstances();
+void printInstance(struct Instance instance);
 
-struct Instance{
-	int max_x, max_y, num_pt, *points;
-};
 
 int main(int argc, char *argv[]){
 
-//check if 5 arguemtns, take either i or o first
-//else if only three check if its i and not o
-//else if only 1 generate nd print to screen
-//else print valid options
 	if(argc==5){
 		if(strcmp(argv[1],"-i")==0 && strcmp(argv[3], "-o")==0){
 			openFile(argv[2], false);
 		}else if(strcmp(argv[1],"-o")==0 && strcmp(argv[3],"-i")==0){
 			openFile(argv[4], false);
 		}else printValidOptions();
+
 	}else if (argc==3 && strcmp(argv[1],"-i")==0){
 		openFile(argv[2], true);
+
 	}else if(argc==1){
-		//gnerate and print to screen
+		 generateInstances();
+
 	}else printValidOptions();
 	
 
@@ -55,9 +57,9 @@ int inRange(int val, int min, int max){
 }
 
 // Checks if given array contains a point
-int contains(int array[][2], int x, int y){
-	for(int p=0; p<sizeof(array); p++ )
-		if(array[p][0]==x && array[p][1]==y)
+int contains(int array[], int x, int y){
+	for(int p=0; p<sizeof(array)/sizeof(int); p+=2 )
+		if(array[p]==x && array[p+1]==y)
 			return true;
 	return false;
 }
@@ -65,6 +67,20 @@ int contains(int array[][2], int x, int y){
 // Prints out the valid options and paramters for the program
 void printValidOptions(){
 	printf("%s [-i inputfile [-o outputfile]]\n",PROGNAME);
+}
+
+//Prints the instance information
+void printInstance(struct Instance instance){
+	printf("%d %d\n", instance.x, instance.y);
+	printf("%d\n", instance.num_pt);
+
+	for(int point=0; point<instance.num_pt*2; point+=2){
+		//why is this giving segmentation error?
+		//works when reading an existing file,
+		//why not when creating?	
+		printf("%d %d\n", *(instance.points+point), *(instance.points+point+1));
+	}
+
 }
 
 void openFile(char fileName[], bool print){
@@ -76,34 +92,33 @@ void openFile(char fileName[], bool print){
 		char str[128];
 		const char delim[2]=" ";
 
-		int num_pt, max_x, max_y, mode=0, point=0;
+		int mode=0, point=0;
 		
-		Instance instance; //Unknown type?
-		int *points;
-	
 		while(fgets(str, sizeof(str), file) != NULL){
 			if(strncmp(str,"#",1) == 0)
 				 continue;
 
 			if(mode == 0){
-				instance->max_x = atoi(strtok(str, delim));
-				max_y = atoi(strtok(NULL,delim));
-				if(print)printf("%d %d\n", instance->max_x, max_y);
+				instance.x = atoi(strtok(str, delim));
+				instance.y = atoi(strtok(NULL,delim));
 				mode++;
 			}else if(mode == 1){
-				num_pt = atoi(strtok(str,delim));
-				if(print)printf("%d\n", num_pt);
+				instance.num_pt = atoi(strtok(str,delim));
 				mode++;
-				points = (int *)malloc(2*num_pt*sizeof(int)); 
+				instance.points = (int *)malloc(2*instance.num_pt*sizeof(int)); 
 			}else {			
-				*(points+point  ) = atoi(strtok(str, delim));
-				*(points+point+1) = atoi(strtok(NULL,delim));
-				if(print)printf("%d %d\n", *(points+point), *(points+point+1));
+				*(instance.points+point  ) = atoi(strtok(str, delim));
+				*(instance.points+point+1) = atoi(strtok(NULL,delim));
 				point+=2;
+				//printf("%d %d\n", *(instance.points+point), *(instance.points+point+1));
 			}
 		}
+
+		if(print) printInstance(instance);
+
+		return instance;
 	} else printf("Error in reading the instance file!\n");
-			//Perhaps add proper error handlng later?
+		//Perhaps add proper error handlng later?
 }
 
 void saveFile(char fileName[]){
@@ -118,40 +133,40 @@ void saveFile(char fileName[]){
 -saves instances
 -prints generation progress
 */
-void generateInstances(){
-	int max_x, max_y, num_pt, inst_ammount;
+void generateInstances(){;
+	
+	int inst_ammount;
 
 	printf("Generating random instances\n");
 	printf("Enter the circuit board size MAX_X MAX_Y: ");
-	scanf("%d %d", &max_x, &max_y );
+	scanf("%d %d", &instance.x, &instance.y);
 	printf("Enter the number of points NUM_PT: ");
-	scanf("%d", &num_pt);
+	scanf("%d", &instance.num_pt);
 	printf("Enter the number of random instances to be generated: ");
 	scanf("%d", &inst_ammount);
 
-	if(max_x * max_y <num_pt)
+	if(instance.x * instance.y < instance.num_pt)
 		printf("Error in generating instances!\n");
 	else {
 
 		for(int j=0; j<inst_ammount; j++){
 
-			int points[num_pt][2];	
-
-			for(int p=0, x_cord, y_cord; p<num_pt; p++){
-				do{
-					x_cord = rand() % max_x-1 + MIN_X;
-					y_cord = rand() % max_y-1 + MIN_Y;
-				}while( !contains(points, x_cord, y_cord) );
-
-				points[p][0] = x_cord;
-				points[p][1] = y_cord;
+			for(int point=0, x_cord, y_cord; point<instance.num_pt; point+=2){
+					do{
+					x_cord = rand() % instance.x-1 + MIN_X;
+					y_cord = rand() % instance.y-1 + MIN_Y;
+				}while( contains(points, x_cord, y_cord) );
+				// CHange containment check?
+				//why exactly does this not work here?			
+				*(instance.points+point  ) = x_cord;
+				*(instance.points+point+1) = y_cord;
 			
 			}
 			//saveFile(saveFileLocation);
-			printf("instance%d_%d.txt generated", num_pt, j+1 );;
+			printInstance(instance);
+			printf("instance%d_%d.txt generated", instance.num_pt, j+1 );;
 			if(j!=inst_ammount-1) printf("\n");
 			
-
 		}
 		printf(" ... done!\n");
 	}
